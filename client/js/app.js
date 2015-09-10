bb41e32fa20781e4cf3f23f10040cbb29b49bfb5
josephnewell90 <josephnewell90@gmail.com>
2015-09-10 16:36:39 -0600

    V.01
    Controller not yet set up

---
+++ client/js/app.js
@@ -0,0 +1,28 @@
+//Declare angular app
+angular.module('RepsApp', []);
+//Make Representative search controller
+angular
+  .module('RepsApp')
+  .controller ([function() {
+    var self = this;
+    //Keep track of the members to display
+    self.members = [];
+  }]);
+
+  //Get the Data
+  angular
+    .module ('RepsApp')
+    .controller(['$http', function ($http) { //This adds the http service
+      var self = this; //Get the members
+      self.members = []; //Dump the members into an array
+
+      //Update the data based on user input for ZIP
+      self.searchByZip = function (zip) {
+        $http
+          .get('http://dgm-representatives.herokuapp.com/all/by-zip/' + zip)
+          .then(function (res) {
+            self.members = res.data;
+          });
+      };
+
+    }]);
