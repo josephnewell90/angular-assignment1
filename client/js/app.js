f0a90f144b32038dffc0c233d9aadbbcb78c9adb
josephnewell90 <josephnewell90@gmail.com>
2015-09-10 19:59:00 -0600

    Zip working, other two not
    For some reason the name and state searches aren't working. On the
    heroku app, the name search doesn't seem to work either?

--- client/js/app.js
+++ client/js/app.js
@@ -1,28 +1,50 @@
+(function()
+{
+
 //Declare angular app
 angular.module('RepsApp', []);
+
 //Make Representative search controller
 angular
   .module('RepsApp')
-  .controller ([function() {
+  .controller('RepsController', [function() {
     var self = this;
     //Keep track of the members to display
     self.members = [];
   }]);
 
-  //Get the Data
-  angular
-    .module ('RepsApp')
-    .controller(['$http', function ($http) { //This adds the http service
-      var self = this; //Get the members
-      self.members = []; //Dump the members into an array
+//Get the Data
+angular
+  .module('RepsApp')
+  .controller('RepsController', ['$http', function ($http) { //This adds the http service
+    var self = this; //Get the members
+    self.members = []; //Dump the members into an array
 
-      //Update the data based on user input for ZIP
-      self.searchByZip = function (zip) {
-        $http
-          .get('http://dgm-representatives.herokuapp.com/all/by-zip/' + zip)
-          .then(function (res) {
-            self.members = res.data;
-          });
-      };
+    //Update the data based on user input for ZIP
+    //Zip
+    self.searchByZip = function (zip) {
+      $http
+        .get('http://dgm-representatives.herokuapp.com/all/by-zip/' + zip)
+        .then(function (res) {
+          self.members = res.data;
+        });
+    };
+    //State
+    self.searchByState = function (state) {
+      $http
+        .get('http://dgm-representatives.herokuapp.com/all/by-state/' + state)
+        .then(function (res) {
+          self.members = res.data;
+        });
+    };
+    //Name
+    self.searchByName = function (name) {
+      $http
+        .get('http://dgm-representatives.herokuapp.com/all/by-name/' + name)
+        .then(function (res) {
+          self.members = res.data;
+        });
+    };
 
-    }]);
+  }]);
+})();
