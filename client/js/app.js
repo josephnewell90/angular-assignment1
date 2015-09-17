7f2da07e1e4f3b2b7f797a8a026656657306cf0f
josephnewell90 <josephnewell90@gmail.com>
2015-09-17 16:16:28 -0600

    Fixed comments bug and added Senator search option
    - Removed comments from app.js because they were breaking things...for
      some reason
    - Added senator search functions and senator factories in app.js
    - Added senator search fields to index.html  as well as clarifying headers

--- client/js/app.js
+++ client/js/app.js
@@ -1,25 +1,42 @@
-//Declare angular app
 angular.module('RepsApp', [
   'RepsAppControllers'
 ]);
-//Make Representative search controller
+
 angular
   .module('RepsAppControllers', [
     'repsService'
   ])
   .controller ('MainCtrl', function (reps) {
-    var main = this; //Get the reps
-    main.reps = []; //Put them into an array
+    var main = this;
+    main.results = [];
 
     main.searchByZip = function (zip) {
       reps.allByZip(zip).then(function (data) {
-        main.reps = data;
+        main.results = data;
       });
     };
 
     main.searchRepsByName = function (name) {
       reps.repsByName(name).then(function (data) {
-        main.reps = data;
+        main.results = data;
+      });
+    };
+
+    main.searchRepsByState = function (state) {
+      reps.repsByState(state).then(function (data) {
+        main.results = data;
+      });
+    };
+
+    main.searchSenatorsByName = function (name) {
+      reps.sensByName(name).then(function (data) {
+        main.results = data;
+      });
+    };
+
+    main.searchSenatorsByState = function (state) {
+      reps.sensByState(state).then(function (data) {
+        main.results = data;
       });
     };
 
@@ -29,8 +46,7 @@ angular
   .module('repsService', [])
   .factory('reps', function ($http) {
     var host = 'http://dgm-representatives.herokuapp.com';
-    return
-    {
+    return {
       allByZip: function (zip) {
         return $http
           .get(host + '/all/by-zip/' + zip)
@@ -38,10 +54,30 @@ angular
             return response.data;
           });
       },
-      repsByName: function (name)
-      {
+      repsByName: function (name) {
         return $http
           .get(host + '/reps/by-name/' + name)
+          .then(function (response) {
+            return response.data;
+          });
+      },
+      repsByState: function (state) {
+        return $http
+          .get(host + '/reps/by-state/' + state)
+          .then(function (response) {
+            return response.data;
+          });
+      },
+      sensByName: function (name) {
+        return $http
+          .get(host + '/sens/by-name/' + name)
+          .then(function (response) {
+            return response.data;
+          });
+      },
+      sensByState: function (state) {
+        return $http
+          .get(host + '/sens/by-state/' + state)
           .then(function (response) {
             return response.data;
           });
