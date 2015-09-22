69132875ed9e15ec5fe483570f795bd7abc715f1
ksmithut <ksmith.ut@gmail.com>
2015-09-21 19:25:26 -0600

    finishes assignment 1

--- client/js/app.js
+++ client/js/app.js
@@ -22,26 +22,56 @@ angular
         main.reps = data;
       });
     };
+
+    main.searchRepsByState = function (state) {
+      reps.repsByState(state).then(function (data) {
+        main.reps = data;
+      });
+    };
+
+    main.searchSensByName = function (name) {
+      reps.sensByName(name).then(function (data) {
+        main.reps = data;
+      });
+    };
+
+    main.searchSensByState = function (state) {
+      reps.sensByState(state).then(function (data) {
+        main.reps = data;
+      });
+    };
   });
 
 angular
   .module('repsService', [])
   .factory('reps', function ($http) {
     var host = 'http://dgm-representatives.herokuapp.com';
+    function returnData(response) { return response.data; }
     return {
       allByZip: function (zip) {
         return $http
           .get(host + '/all/by-zip/' + zip)
-          .then(function (response) {
-            return response.data;
-          });
+          .then(returnData);
       },
       repsByName: function (name) {
         return $http
           .get(host + '/reps/by-name/' + name)
-          .then(function (response) {
-            return response.data;
-          });
+          .then(returnData);
+      },
+      repsByState: function (state) {
+        return $http
+          .get(host + '/reps/by-state/' + state)
+          .then(returnData);
+      },
+      sensByName: function (name) {
+        return $http
+          .get(host + '/sens/by-name/' + name)
+          .then(returnData);
+      },
+      sensByState: function (state) {
+        return $http
+          .get(host + '/sens/by-state/' + state)
+          .then(returnData);
       }
     };
   });
